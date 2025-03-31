3c953da5d04350c2a6d6252db964c30fd8eded2c
Kailin O'Donnell <kailin.odonnell.it3@gmail.com>
2025-03-31 18:57:22 +0100

    add broadcast status

---
+++ nem2-sdk-csharp/src/Infrastructure/HttpRepositories/Responses/BroadcastStatus.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace io.nem2.sdk.src.Infrastructure.HttpRepositories.Responses
+{
+    public class BroadcastStatus
+    {
+        public string Status { get; set; }
+    }
+
+    public class ExtendedBroadcastStatus
+    {
+        public string Group { get; set; }
+        public string Code { get; set; }
+        public string Hash { get; set; }
+        public string Deadline { get; set; }
+        public string Height { get; set; }
+    }
+}
