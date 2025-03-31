5a22aee7583ba66a17384fcc87c20868f5ffd89f
Kailin O'Donnell <kailin.odonnell.it3@gmail.com>
2025-03-31 18:56:00 +0100

    add data serializer

---
+++ Unit-Tests/TransactionSerialization/DataSerializer.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+using io.nem2.sdk.src.Infrastructure.Buffers.NativeBuffer;
+
+namespace Unit_Tests.TransactionSerialization
+{
+    internal class DataSerializerTests
+    {
+        [Test]
+        public void Serializer()
+        {
+            var serializer = new TransactionDataSerializer(8 + 32 + 4 + 2 + 1);
+
+            serializer.WriteUlong(10000000000);
+            serializer.WriteHexString("");
+            serializer.WriteUint(10000);
+            serializer.WriteUshort(100);
+            serializer.WriteByte(1);
+
+            Debug.WriteLine(Convert.ToHexString(serializer.Bytes));
+        }
+    }
+}
---
+++ nem2-sdk-csharp/src/Infrastructure/Buffers/NativeBuffer/TransactionDataSerializer.cs
@@ -0,0 +1,60 @@
+﻿namespace io.nem2.sdk.src.Infrastructure.Buffers.NativeBuffer
+{ 
+    public class TransactionDataSerializer
+    {
+        public byte[] Bytes;
+        public int _offset = 0;
+
+        public TransactionDataSerializer(int transactionSize)
+        {
+            Bytes = new byte[transactionSize];
+        }
+
+        public void WriteUlong(ulong data)
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                Bytes[_offset + i] = (byte)(data >> (8 - 1 - i) * 8);
+            }
+
+            _offset += 8;
+        }
+
+        public void WriteUint(uint data)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                Bytes[_offset + i] = (byte)(data >> (4 - 1 - i) * 8);
+            }
+
+            _offset += 4;
+        }
+
+        public void WriteUshort(ushort data)
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                Bytes[_offset + i] = (byte)(data >> (2 - 1 - i) * 8);
+            }
+
+            _offset += 2;
+        }
+
+        public void WriteByte(byte data)
+        {
+            Bytes[_offset] = data;
+
+            _offset += 1;
+        }
+
+        public void WriteHexString(string data)
+        {
+            for (var i = 0; i < data.Length / 2; i++)
+            {
+                Bytes[_offset + i] = Convert.ToByte(data.Substring(i * 2, 2), 16);
+            }
+
+            _offset += data.Length / 2;
+        }
+    }
+}
