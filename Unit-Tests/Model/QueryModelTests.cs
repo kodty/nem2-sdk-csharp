48ac754a5d2ad11f653961c5264cf13f3f1e1e55
Kailin <kailin.odonnell.it3@gmail.com>
2025-06-13 20:31:06 +0200

    Add query model tests - unfinished

--- Unit-Tests/Model/QueryModelTests.cs
+++ Unit-Tests/Model/QueryModelTests.cs
@@ -81,9 +81,19 @@ namespace Unit_Tests.Model
             queryModelArray[7].SetParam(QueryModel.DefinedParams.scopedMetadataKey, "501EA9C5BF005AE1");
             queryModelArray[7].SetParam(QueryModel.DefinedParams.targetId, "0A795B69698C01D6");
             queryModelArray[7].SetParam(QueryModel.DefinedParams.metadataType, 1);
-            //queryModelArray[7].SetParam(QueryModel.DefinedParams.pageSize, 10);
-            //queryModelArray[7].SetParam(QueryModel.DefinedParams.pageNumber, 1);
-           // queryModelArray[7].SetParam(QueryModel.DefinedParams.order, QueryModel.Order.Asc);
+
+            queryModelArray[8].SetParam(QueryModel.DefinedParams.mosaicId, "613E6D0FC11F4530");
+            queryModelArray[8].SetParam(QueryModel.DefinedParams.entryType, 0);
+            queryModelArray[8].SetParam(QueryModel.DefinedParams.targetAddress, "NAT6KCGFXOBBVK2FIFKH2AYHE6G6G6EENK4CJVY");
+            queryModelArray[8].SetParam(QueryModel.DefinedParams.pageSize, 1);
+            queryModelArray[8].SetParam(QueryModel.DefinedParams.pageNumber, 1);
+            queryModelArray[8].SetParam(QueryModel.DefinedParams.order, QueryModel.Order.Asc);
+
+            queryModelArray[9].SetParam(QueryModel.DefinedParams.ownerAddress, "NASYMBOLLK6FSL7GSEMQEAWN7VW55ZSZU25TBOA");
+            queryModelArray[9].SetParam(QueryModel.DefinedParams.pageSize, 1);
+            queryModelArray[9].SetParam(QueryModel.DefinedParams.pageNumber, 1);
+            //queryModelArray[8].SetParam(QueryModel.DefinedParams.offset, "");
+            queryModelArray[9].SetParam(QueryModel.DefinedParams.order, QueryModel.Order.Asc);
 
             Assert.That(queryModelArray[0].ReturnPathParams(), Is.EqualTo("height=78882&pageSize=10&pageNumber=1&order=Asc")); // see why receipt height at 923
             Assert.That(queryModelArray[1].ReturnPathParams(), Is.EqualTo("height=1126&pageSize=10&pageNumber=1&order=Asc"));
@@ -93,7 +103,8 @@ namespace Unit_Tests.Model
             Assert.That(queryModelArray[5].ReturnPathParams(), Is.EqualTo("recipientAddress=68F35B3CA84DB724B948E7A6B2DAB065E2FD4D51139BA3A6&signerPublicKey=90E1D2A533D6715235CB49CFBD69EE0A69B8F89C8FD74C02546E5A9E54498F80&height=79760&fromHeight=79760&toHeight=79760&pageNumber=1&pageSize=10"));
             Assert.That(queryModelArray[6].ReturnPathParams(), Is.EqualTo("address=NASYMBOLLK6FSL7GSEMQEAWN7VW55ZSZU25TBOA&pageSize=10&pageNumber=1&order=Asc")); // see why receipt height at 923
             Assert.That(queryModelArray[7].ReturnPathParams(), Is.EqualTo("sourceAddress=68D59ED4096C5CF366F65E554C7BC0FABEA0E60D4B28FB4A&targetAddress=68D59ED4096C5CF366F65E554C7BC0FABEA0E60D4B28FB4A&scopedMetadataKey=501EA9C5BF005AE1&targetId=0A795B69698C01D6&metadataType=1"));
-
+            Assert.That(queryModelArray[8].ReturnPathParams(), Is.EqualTo("mosaicId=613E6D0FC11F4530&entryType=0&targetAddress=NAT6KCGFXOBBVK2FIFKH2AYHE6G6G6EENK4CJVY&pageSize=1&pageNumber=1&order=Asc"));
+            Assert.That(queryModelArray[9].ReturnPathParams(), Is.EqualTo("ownerAddress=NASYMBOLLK6FSL7GSEMQEAWN7VW55ZSZU25TBOA&pageSize=1&pageNumber=1&order=Asc"));
         }
     }
 }
