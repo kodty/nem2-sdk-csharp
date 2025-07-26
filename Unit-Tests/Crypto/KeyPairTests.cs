15ca88effe61938ae3e1e71307b53c145f17e99c
Kailin O'Donnell <kailin.odonnell.it3@gmail.com>
2025-07-26 17:58:14 +0200

    Add secretKeyPair test vectors

---
+++ Unit-Tests/Crypto/KeyPairTests.cs
@@ -0,0 +1,25 @@
+﻿using io.nem2.sdk.src.Model2;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using System.Text.Json.Nodes;
+
+namespace Unit_Tests.Crypto
+{
+    internal class KeyPairTests
+    {
+        [Test]
+        public static void TestKeyPair()
+        {
+            var text = File.ReadAllLines("C:\\Users\\kaili\\Documents\\Bitbucket-Repositories\\nem2-sdk-csharp\\Unit-Tests\\Crypto\\Vectors\\TextFile1.txt");
+
+            var keys = JsonObject.Parse(String.Concat(text));
+
+            for (var i = 0; i < 10000; i++)
+            {
+                var kp = SecretKeyPair.CreateFromPrivateKey(keys[i]["privateKey"].GetValue<string>());
+
+                Assert.That(kp.PublicKeyString, Is.EqualTo(keys[i]["publicKey"].GetValue<string>()));
+            }   
+        }   
+    }
+}
