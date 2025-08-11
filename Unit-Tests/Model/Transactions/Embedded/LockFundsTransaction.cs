a6e0f8be5dda75f5e53540f5aa03ea61573ad1d7
Kailin O'Donnell <kailin.odonnell.it3@gmail.com>
2025-08-12 00:03:18 +0200

    no message

--- Unit-Tests/Model/Transactions/Embedded/LockFundsTransaction.cs
+++ Unit-Tests/Model/Transactions/Embedded/LockFundsTransaction.cs
@@ -1,13 +1,14 @@
 ﻿using Coppery;
 using Integration_Tests;
 using io.nem2.sdk.src.Model;
+using System.Diagnostics;
 
 namespace Unit_Tests.Model.Transactions.Embedded
 {
     internal class LockFundsTransaction
     {
         [Test, Timeout(20000)]
-        public async Task CreateLockFundsTest()
+        public async Task CreateLockFundsEmbeddedTest()
         {
             var keys = SecretKeyPair.CreateFromPrivateKey("98AA70CA43E5D3B95CD303A57892D0BA953C204A4D937AF4386ED658A8FA555D");
 
@@ -16,12 +17,12 @@ namespace Unit_Tests.Model.Transactions.Embedded
                     new Tuple<string, ulong>("72C0212E67A08BCE", 101),
                     1440,
                     "98AA70CA43E5D3B95CD303A57892D0BA953C204A4D937AF4386ED658A8FA555D",
-                    false
+                    true
                 );
 
-            var result = lockFunds.WrapVerified(keys);
+            var result = lockFunds.Embed(keys.PublicKeyString);
 
-            Assert.That(result.Payload.ToHex(), Is.EqualTo("B800000000000000EE8E77731A11BCB058A3029DC0223EF4ADC024C8F5B82C7485EACB9AB4AA1C0DD6E5B6C58951A8B6F84D98BF8EC6470232E93B2BC7307BF7A583CA62A9105A0691D5DCB54E185D3700DD88283D9DC8C3EDC58A18305BB2B933BBA252B516B45200000000019848416A5D22B404000000B52E115A0200000072C0212E67A08BCE6500000000000000A00500000000000098AA70CA43E5D3B95CD303A57892D0BA953C204A4D937AF4386ED658A8FA555D"));
+            Assert.That(result.Payload.ToHex(), Is.EqualTo("680000000000000091D5DCB54E185D3700DD88283D9DC8C3EDC58A18305BB2B933BBA252B516B452000000000198484172C0212E67A08BCE6500000000000000A00500000000000098AA70CA43E5D3B95CD303A57892D0BA953C204A4D937AF4386ED658A8FA555D"));
         }
     }
 }
