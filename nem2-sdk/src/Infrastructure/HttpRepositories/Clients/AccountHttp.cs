e1a788eeeff6f0965ed5c829837c203197c85869
Kailin O'Donnell <kailin.odonnell.it3@gmail.com>
2026-07-04 01:16:15 +0100

    more compact format

--- nem2-sdk/src/Infrastructure/HttpRepositories/Clients/AccountHttp.cs
+++ nem2-sdk/src/Infrastructure/HttpRepositories/Clients/AccountHttp.cs
@@ -8,46 +8,33 @@ namespace io.nem2.sdk.src.Infrastructure.HttpRepositories.Clients
 {
     public class AccountHttp : HttpRouter, IAccountRepository
     {
-        public AccountHttp(string host, int port) : base(host, port)
-        {
-            
-        }
+        public AccountHttp(string host, int port) : base(host, port){}
    
         public IObservable<ExtendedHttpResponseMessege<Datum<AccountData>>> SearchAccounts(QueryModel queryModel)
-        {
-            return HttpGetAsync<Datum<AccountData>>(queryModel, ["accounts"]);
-        }
+            => HttpGetAsync<Datum<AccountData>>(queryModel, ["accounts"]);
 
         public IObservable<ExtendedHttpResponseMessege<AccountData>> GetAccount(string pubkOrAddress)
-        {
-            return HttpGetAsync<AccountData>(["accounts", pubkOrAddress]);          
-        }
+            => HttpGetAsync<AccountData>(["accounts", pubkOrAddress]);          
 
-        public IObservable<ExtendedHttpResponseMessege<AccountData[]>> GetAccounts(List<string> accounts) // flag
-        {
-            var content = new StringContent(JsonSerializer.Serialize(new Public_Keys() { publicKeys = accounts }), Encoding.UTF8, "application/json");
-
-            return HttpPostAsync<AccountData[]>(["accounts"], content);
-        }
+        public IObservable<ExtendedHttpResponseMessege<AccountData[]>> GetAccounts(List<string> accounts)
+            => HttpPostAsync<AccountData[]>(["accounts"], 
+                new StringContent(
+                    JsonSerializer.Serialize(
+                        new Public_Keys() { publicKeys = accounts }), 
+                        Encoding.UTF8, 
+                        "application/json"
+                    ));      
 
         public IObservable<ExtendedHttpResponseMessege<MerkleRoot>> GetAccountMerkle(string pubkOrAddress)
-        {
-            return HttpGetAsync<MerkleRoot>(["accounts", pubkOrAddress, "merkle"]);
-        }
+            => HttpGetAsync<MerkleRoot>(["accounts", pubkOrAddress, "merkle"]);
 
         public IObservable<ExtendedHttpResponseMessege<RestrictionsData>> SearchAccountRestrictions(QueryModel queryModel)
-        {
-            return HttpGetAsync<RestrictionsData>(queryModel, ["restrictions", "account"]);
-        }
+            => HttpGetAsync<RestrictionsData>(queryModel, ["restrictions", "account"]);
 
         public IObservable<ExtendedHttpResponseMessege<RestrictionData>> GetAccountRestriction(string address)
-        {
-            return HttpGetAsync<RestrictionData>(["restrictions", "account", address]);
-        }
+            => HttpGetAsync<RestrictionData>(["restrictions", "account", address]);
 
         public IObservable<ExtendedHttpResponseMessege<MerkleRoot>> GetAccountRestrictionsMerkle(string address)
-        {
-            return HttpGetAsync<MerkleRoot>(["restrictions", "account", address, "merkle"]);          
-        }
+            => HttpGetAsync<MerkleRoot>(["restrictions", "account", address, "merkle"]);                 
     }
 }
