03841234123cf755ccafd7080ec1975e2547c61f
Kailin O'Donnell <kailin.odonnell.it3@gmail.com>
2026-07-10 02:01:38 +0100

    remove some commenting

--- nem2-sdk/src/Model/Accounts/MultisigAccountInfo.cs
+++ nem2-sdk/src/Model/Accounts/MultisigAccountInfo.cs
@@ -13,22 +13,11 @@
         public List<PublicAccount> Cosignatories { get; }
        
         public List<PublicAccount> MultisigAccounts { get; }
-        /// <summary>
-        /// Checks if an account is cosignatory of the multisig account.
-        /// </summary>
-        /// <param name="account">The account.</param>
-        /// <returns><c>true</c> if the specified account has cosigners; otherwise, <c>false</c>.</returns>
+        
         public bool HasCosigners(PublicAccount account) => Cosignatories.Contains(account);
-        /// <summary>
-        /// Checks if the multisig account is cosignatory of an account.
-        /// </summary>
-        /// <param name="account">The account.</param>
-        /// <returns><c>true</c> if [is cosigner of multisig account] [the specified account]; otherwise, <c>false</c>.</returns>
+        
         public bool IsCosignerOfMultisigAccount(PublicAccount account) => MultisigAccounts.Contains(account);
-        /// <summary>
-        /// Checks if the account is a multisig account.
-        /// </summary>
-        /// <value><c>true</c> if this instance is multisig; otherwise, <c>false</c>.</value>
+       
         public bool IsMultisig => MinApproval != 0 && MinRemoval != 0;
 
         
--- nem2-sdk/src/Model/Articles/MosaicProperties.cs
+++ nem2-sdk/src/Model/Articles/MosaicProperties.cs
@@ -12,17 +12,6 @@
 
         public ulong Duration { get; }
 
-
-        /// <summary>
-        /// Constructor
-        /// </summary>
-        /// <param name="supplyMutable">The mosaic supply mutability.</param>
-        /// <param name="transferable">The mosaic transferability.</param>
-        /// <param name="levyMutable">The mosaic levy mutability.</param>
-        /// <param name="divisibility">The mosaic divisibility.</param>
-        /// <param name="duration">The number of blocks the mosaic will be active.</param>
-        /// <exception cref="Exception">Duration must be set</exception>
-        /// <exception cref="ArgumentException">Divisibility must be between 0 and 6</exception>
         public MosaicProperties(bool supplyMutable, bool transferable, bool levyMutable, int divisibility, ulong duration)
         {
             if (divisibility < 0 || divisibility > 6) throw new ArgumentException("Divisibility must be between 0 and 6");
