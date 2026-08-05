5409555c03de7f06d9433d92cc8f25d311e5bf91
Kailin O'Donnell <kailin.odonnell.it3@gmail.com>
2026-08-05 22:20:57 +0100

    delete some usings

--- Integration-Tests/TransactionBroadcastTests/TransferTransactionTests.cs
+++ Integration-Tests/TransactionBroadcastTests/TransferTransactionTests.cs
@@ -1,15 +1,4 @@
-﻿using Coppery;
-using Integration_Tests;
-using io.nem2.sdk.Infrastructure;
-using io.nem2.sdk.Infrastructure.HttpClients;
-using io.nem2.sdk.Model;
-using io.nem2.sdk.Model.Accounts;
-using io.nem2.sdk.Model.Articles;
-using io.nem2.sdk.Model.Transactions.Messages;
-using io.nem2.sdk.Utils;
-using System.Diagnostics;
-using System.Reactive.Linq;
-
+﻿
 namespace IntegrationTests.Infrastructure.Transactions
 {
     public class TransferTransactionTests
