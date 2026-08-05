2589cfa2e34f5b072623baab71a8de6b3372f009
Kailin O'Donnell <kailin.odonnell.it3@gmail.com>
2026-08-05 22:19:49 +0100

    seperate mosaic alias and address alias transaction classes from base alias transaction class

---
+++ nem2-sdk/Model/Transactions/AddressAliasTransaction.cs
@@ -0,0 +1,34 @@
+﻿using Coppery;
+using io.nem2.sdk.Model.Accounts;
+
+namespace io.nem2.sdk.Model.Transactions
+{
+    public class AddressAliasTransaction : AliasTransaction
+    {
+        public AddressAliasTransaction(string address, ulong namespaceId, byte aliasAction) : base(TransactionTypes.Types.ADDRESS_ALIAS, namespaceId, aliasAction)
+        {
+            AddressToAlias = address.IsBase32()
+                      ? Address.DecodeAddress(address)
+                      : address.FromHex();
+        }
+
+        internal override void Extend(DataSerializer serializer)
+        {
+            serializer.SerializeProperty(NamespaceId);
+            serializer.SerializeProperty(AddressToAlias);
+            serializer.SerializeProperty(AliasAction);
+        }
+
+        public byte[] AddressToAlias { get; set; }
+
+        internal override int AddSize()
+        {
+            return 33;
+        }
+
+        internal override TransactionTypes.Types SetType()
+        {
+            return TransactionTypes.Types.ADDRESS_ALIAS;
+        }
+    }
+}
---
+++ nem2-sdk/Model/Transactions/MosaicAliasTransaction.cs
@@ -0,0 +1,31 @@
+﻿using Coppery;
+
+namespace io.nem2.sdk.Model.Transactions
+{
+    public class MosaicAliasTransaction : AliasTransaction
+    {
+        public MosaicAliasTransaction(ulong mosaicId, ulong namespaceId, byte aliasAction) : base(TransactionTypes.Types.MOSAIC_ALIAS, namespaceId, aliasAction)
+        {
+            MosaicId = mosaicId;
+        }
+
+        internal override void Extend(DataSerializer serializer)
+        {
+            serializer.SerializeProperty(NamespaceId);
+            serializer.SerializeProperty(MosaicId);
+            serializer.SerializeProperty(AliasAction);
+        }
+
+        public ulong MosaicId { get; set; }
+
+        internal override int AddSize()
+        {
+            return 17;
+        }
+
+        internal override TransactionTypes.Types SetType()
+        {
+            return TransactionTypes.Types.MOSAIC_ALIAS;
+        }
+    }
+}
